25f244a8d7800413486d0e15c0716cbf2a1cbd35
Seungwoo Son <sswpro@gmail.com>
2018-02-15 17:53:56 -0800

    hw1

--- foobar.rb
+++ foobar.rb
@@ -4,5 +4,30 @@ class Foobar
     # Class method, not an instance method
     # Call with `Foobar.baz`
     # Q4 CODE HERE
+    a = a.uniq
+    a.keep_if { |x| Integer(x)%2 == 0 && Integer(x) < 10}
+    k = a.inject(0){|sum,x| sum + Integer(x) + 2}
+    return k
   end
 end
+
+
+# It's blockin' time!
+
+# In hw1.rb we defined a function called blockin_time 
+# that requires the foobar.rb file, and then calls a class method 'baz' from the Foobar class.
+
+# If you take a look at foobar.rb, 
+# you'll see that we've defined Foobar. 
+# Write a class function in foobar.rb called baz 
+# that accepts an array of integers as strings, 
+# changes each string into an integer, 
+# adds 2 to each number, keeps the even numbers only, 
+# removes any duplicates, rejects the 
+# resulting numbers greater than or equal to ten, and finally returns the sum of the resulting array.
+
+# For example:
+
+# ['1', '2', '3', '4', '5', '10'] returns 10. ((2+2) + (4+2)) = 10.
+
+# ['1', '2', '2', '2', '3', '4', '5', '10'] returns 10 as well.
--- hw1.rb
+++ hw1.rb
@@ -1,16 +1,22 @@
+# There is a method called squared_sum in hw1.rb. 
+# squared_sum accepts two integer parameters a and b. 
+# The method should calculate the sum of a and b and then return the square of the sum. 
+
 def squared_sum(a, b)
-  # Q1 CODE HERE
+  return (a+b)**2
 
 end
 
+ # accepts an array of integers as its parameter. 
+ # The method should sort the given array, increment each element by 1, and finally return it. 
+ # Please finish this method, either in a destructive or non-destructive fashion.
 def sort_array_plus_one(a)
-  # Q2 CODE HERE
-
+  a = a.sort!
+  a.map { |x| x+1 }
 end
 
 def combine_name(first_name, last_name)
-  # Q3 CODE HERE
-
+  return (first_name + " " + last_name)
 end
 
 def blockin_time(a)
@@ -48,5 +54,24 @@ def scrabble(word)
     y: 4,
     z: 10,
   }
-  # Q5 CODE HERE
+  word.split("").inject(0){|sum,c| sum + values[c.to_sym]}
 end
+
+# You want to get a leg up on your next Scrabble game with your grandmother. 
+# Write a function that takes a word (string) and 
+# returns the scrabble value of that word. Use the hash provided in the method to get the values. 
+# Assume the word has all lowercase letters.
+
+# Your code should be on a single line.
+
+# Hint: Try to split the word to get the letters in an array
+
+# Hint: Are :this_one and "this_one" equivalent objects?
+
+# Hint: Look at Array#sum
+
+# Example:
+
+# > scrabble("equestrian")
+# => 19
+# This should get you familiar with basic operators, strings, symbols, and iterators.
